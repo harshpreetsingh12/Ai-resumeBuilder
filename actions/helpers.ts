6ccfbba9fb3209718d340badaa3acf38188ee383
AjaySRawat07 <ajju246455@gmail.com>
2025-01-15 21:59:40 +0530

    threeChanges

--- actions/helpers.ts
+++ actions/helpers.ts
@@ -6,6 +6,7 @@ export const isUserExist = async () => {
     const { userId } = await auth();
     if (!userId) throw new Error("Unauthorized");
 
+    // check existing user
     const user = await db.user.findUnique({
       where: {
         clerkUserId: userId,
