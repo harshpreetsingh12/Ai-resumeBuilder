3778b06e1e0e84d8f33d5b471da72e594f8a138b
unknown <problemsolver131@gmail.com>
2025-01-25 21:15:55 +0530

    color hex issues

--- src/app/(main)/dashboard/page.tsx
+++ src/app/(main)/dashboard/page.tsx
@@ -2,6 +2,8 @@ import React from 'react'
 import DashboardPage from './DashboardPage'
 import { getAllUserResumes } from '../../../../actions/resume'
 
+export const dynamic = 'force-dynamic';
+
 const page = async () => {
   const AllResumes= await getAllUserResumes()
   return <DashboardPage resumes={AllResumes}/>
